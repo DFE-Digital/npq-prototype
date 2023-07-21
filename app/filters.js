a5631ef4fc22c416b48e14cef9efd82451bece3b
Jackie DAlcorn <jacqueline@dalcorn.com>
2023-07-21 16:56:11 +0100

    adding submitted date to registration details pages. Adding date filter to prototype so we can use today etc.

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,21 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+const prototypeFilters = require('@x-govuk/govuk-prototype-filters');
+
+  module.exports = function (env) {
+    /**
+     * Instantiate object used to store the methods registered as a
+     * 'filter' (of the same name) within nunjucks. You can override
+     * gov.uk core filters by creating filter methods of the same name.
+     * @type {Object}
+     */
+    var filters = prototypeFilters
+
+    // Existing filter
+    filters.sayHi = function(name) {
+        return 'Hi ' + name + '!'
+    }
+
+    return filters
+  }
