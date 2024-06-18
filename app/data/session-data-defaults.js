dbde2df9009f2113938e0fa6d0145d0f01c2a56e
Jackie DAlcorn <jacqueline@dalcorn.com>
2024-06-18 17:43:11 +0100

    design changes for edge case review in admin tool

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -22,4 +22,47 @@ module.exports = {
   // Insert values here
   'dia': 'DfE Identity account',
 
+
+  // For admin console 
+  // Applicant 1
+  'applicant1--name': 'Annamaria Green',
+  'applicant1--email': 'annamaria.green@example.com',
+  'applicant1--referred': 'No',
+  'applicant1--employment-type': 'In a young offender institution',
+  'applicant1--employer': 'Kent county council',
+  'applicant1--role': 'Receptionist',
+  'applicant1--course': 'Special educational needs co-ordinator (SENCO)',
+  'applicant1--provider': 'Ambition Institute',
+  'applicant1--review-status': 'Decision made',
+  'applicant1--funding-status': 'No',
+  'applicant1--ineligible-reason': 'Not in a teaching role',
+  'applicant1--ineligible-reason--other': '',
+  'applicant1--ineligible-reason--establishment': '',
+  'applicant1--provider-status': 'Pending',
+  'applicant1--notes': '15/04: NG sent email sent to user to confirm non-eligibility. 12/04: CD Agree not eligible. 11/04: AB Not elgible - in support role, not teaching.',
+  'applicant1--submitted-date': '10 Feb 2023 2:30pm',
+  'applicant1--updated-date': '12 Feb 2023 12:55pm',
+  'applicant1--participant-id': '50558b2a-db55-40e0-ae99-ff02f9065ef7',
+  'applicant1--applicant-id': '50558b2a-db55-40e0-ae99-ff02f9065ef7',
+
+  // Applicant 2
+  'applicant2--name': 'Lynne Howe',
+  'applicant2--email': 'l.howe@example.com',
+  'applicant2--referred': 'No',
+  'applicant2--employment-type': 'Virtual school',
+  'applicant2--employer': 'A1 school',
+  'applicant2--role': 'Math teacher',
+  'applicant2--course': 'NPQ Leading teacher development (NPQLTD)',
+  'applicant2--provider': 'Teach First',
+  'applicant2--review-status': 'Awaiting information',
+  'applicant2--funding-status': '–',
+  'applicant2--ineligible-reason': '',
+  'applicant2--ineligible-reason--other': '',
+  'applicant2--ineligible-reason--establishment': '',
+  'applicant2--provider-status': 'Pending',
+  'applicant2--notes': '10/04: JD Requested more info on place of work and location. 8/04: AB looks eligible but need to check school.',
+  'applicant2--submitted-date': '8 Apr 2024 3:45pm',
+  'applicant2--updated-date': '12 Feb 2023 13:18pm',
+  'applicant2--participant-id': '50558b2a-db55-40e0-ae99-ff02f9065ef7',
+  'applicant2--applicant-id': '50558b2a-db55-40e0-ae99-ff02f9065ef7'
 }
--- app/routes/v2-routes-admin.js
+++ app/routes/v2-routes-admin.js
@@ -1,17 +1,54 @@
 const _ = require('lodash')
 
 module.exports = router => {
+  // Applicant 1 
+  // update review status 
+  router.post('/applicant-1--review-status', function(req, res){
+    var appp1reviewstatus = req.session.data['applicant1--review-status']
 
-  router.post('/review-status', function(req, res){
-    var reviewstatus = req.session.data['adminreviewstatus']
+    if (appp1reviewstatus == 'Decision made') {
+      res.redirect('/admin-console/v2/applicant-1/set-eligibility')
+    } else if ((appp1reviewstatus == 'Needs review') || (appp1reviewstatus == 'Awaiting information') || (appp1reviewstatus == 'Re-register')){
+      res.redirect('/admin-console/v2/applicant-1/set-notes')
+    } else {
+      res.redirect('/admin-console/v2/applicant-1/applicant')
+    }
+  })
 
-    if (reviewstatus == 'Decision made') {
+  // update eligibility status 
+  router.post('/applicant-1--eligibility-status', function(req, res){
+    var appp1fundingstatus = req.session.data['applicant1--funding-status']
+
+    if (appp1fundingstatus == 'No') {
+      res.redirect('/admin-console/v2/applicant-1/set-ineligible-reason')
+    } else {
+      res.redirect('/admin-console/v2/applicant-1/set-notes')
+    } 
+  })
+
+  // Applicant 2 
+  // update review status 
+  router.post('/applicant-2--review-status', function(req, res){
+    var appp2reviewstatus = req.session.data['applicant2--review-status']
+
+    if (appp2reviewstatus == 'Decision made') {
       res.redirect('/admin-console/v2/applicant-2/set-eligibility')
-    } else if ((reviewstatus == 'In progress') || (reviewstatus == 'Awaiting information') || (reviewstatus == 'Re-register')){
+    } else if ((appp2reviewstatus == 'Needs review') || (appp2reviewstatus == 'Awaiting information') || (appp2reviewstatus == 'Re-register')){
       res.redirect('/admin-console/v2/applicant-2/set-notes')
     } else {
       res.redirect('/admin-console/v2/applicant-2/applicant')
     }
+  })
+
+  // update eligibility status 
+  router.post('/applicant-2--eligibility-status', function(req, res){
+    var appp2fundingstatus = req.session.data['applicant2--funding-status']
+
+    if (appp2fundingstatus == 'No') {
+      res.redirect('/admin-console/v2/applicant-2/set-ineligible-reason')
+    } else {
+      res.redirect('/admin-console/v2/applicant-2/set-notes')
+    } 
   })
 
 }
