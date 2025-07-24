b0a2384fa8fe07d2e23ec1773ab11ea89f48ccbd
siddiqah-ii <siddiqahiislam@gmail.com>
2025-07-24 10:50:04 +0100

    orignal routes
    router.get('/user-research/npq/new-user', (req, res) => {
        const data = req.session.data
        data.signedIn = 'true'
        res.redirect(v + 'course-start')
      })

--- app/routes/v3/routes.js
+++ app/routes/v3/routes.js
@@ -11,13 +11,11 @@ module.exports = router => {
 // ------------
 
   // Redirect from Identity prototype - NPQ create account
- 
-
-
-router.get('/user-research/npq/new-user', (req, res) => {
-  req.session.data = { signedIn: 'true' }; // Keep it minimal
-  res.redirect(v + 'course-start');
-});
+  router.get('/user-research/npq/new-user', (req, res) => {
+    const data = req.session.data
+    data.signedIn = 'true'
+    res.redirect(v + 'course-start') 
+  })
 
 
   // Redirect from Identity prototype - NPQ show account
